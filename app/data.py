ece3fe1453a4599ab4f26f4be5fc157a63fd3760
Colin Maudry <colin@maudry.com>
2022-04-28 17:22:37 +0200

    Validation du numéro SIRET entrer avant de requêter

--- app/data.py
+++ app/data.py
@@ -64,13 +64,35 @@ def get_data(value: str) -> str:
 
 @dash_app.callback(
     Input('query-result', 'data'),
+    Input('siret', 'value'),
     output=dict(
         bsdd_graphiques_col=Output('bsdd_graphiques_col', 'children'),
         poids_departement_recus=Output('poids_departement_recus_col', 'children'),
     )
 )
-def get_bsdd_figures(json_data: str):
+def get_bsdd_figures(json_data: str, siret: str):
+    # Is SIRET 14 char long?
+    if len(siret) != 14:
+        return {
+            "bsdd_graphiques_col": [
+                dbc.Col([], width={'size': 4, 'offset': 4})
+            ],
+            "poids_departement_recus": ""
+        }
     df: pd.DataFrame = pd.read_json(json_data, orient='split', convert_dates=['mois'])
+
+    # Are there any BSDD in the dataframe?
+    if df.index.size == 0:
+        return {
+            "bsdd_graphiques_col": [
+                dbc.Col([
+                    "Pas de bordereaux entrés dans Trackdéchets pour cet établissement sur la période, "
+                    "ni comme émetteur, "
+                    "ni comme destinataire."
+                ], width={'size': 4, 'offset': 4})
+            ],
+            "poids_departement_recus": ""
+        }
 
     emis = df.query('origine == "émis"')
     recus = df.query('origine == "reçus"')
@@ -97,130 +119,122 @@ def get_bsdd_figures(json_data: str):
     # BSDD / origine / poids / mois
     #
 
-    if df.index.size > 0:
-        df["poids"] = df.apply(
-            normalize_quantity_received, axis=1
-        )
+    df["poids"] = df.apply(
+        normalize_quantity_received, axis=1
+    )
 
-        bsdd_grouped_poids_mois = df[['poids', 'origine', 'mois']].groupby(by=['mois', 'origine'],
-                                                                           as_index=False).sum()
-        bsdd_grouped_poids_mois['mois'] = [dt.strftime(date, "%b/%y") for date in bsdd_grouped_poids_mois['mois']]
-        bsdd_grouped_poids_mois['poids'] = bsdd_grouped_poids_mois['poids'].apply(round)
+    bsdd_grouped_poids_mois = df[['poids', 'origine', 'mois']].groupby(by=['mois', 'origine'],
+                                                                       as_index=False).sum()
+    bsdd_grouped_poids_mois['mois'] = [dt.strftime(date, "%b/%y") for date in bsdd_grouped_poids_mois['mois']]
+    bsdd_grouped_poids_mois['poids'] = bsdd_grouped_poids_mois['poids'].apply(round)
 
-        #
-        # BSDD / reçus / département
-        #
+    #
+    # BSDD / reçus / département
+    #
 
-        df_bsdd_origine_poids: pd.DataFrame = recus[['emitterCompanyAddress', 'poids']].copy()
-        df_bsdd_origine_poids['departement_origine'] = df_bsdd_origine_poids['emitterCompanyAddress'].str. \
-            extract(r"(\d{2})\d{3}")
-        df_bsdd_origine_poids = df_bsdd_origine_poids[['departement_origine', 'poids']]. \
-            groupby(by='departement_origine', as_index=False).sum()
-        df_bsdd_origine_poids['poids'] = df_bsdd_origine_poids['poids'].apply(round)
-        df_bsdd_origine_poids.sort_values(by='poids', ascending=True, inplace=True)
-        df_bsdd_origine_poids = df_bsdd_origine_poids.tail(10)
-        departements = pd.read_csv('app/assets/departement.csv', index_col='DEP')
-        df_bsdd_origine_poids = df_bsdd_origine_poids.merge(departements, left_on='departement_origine',
-                                                            right_index=True)
-        df_bsdd_origine_poids['LIBELLE'] = df_bsdd_origine_poids['LIBELLE'] + ' (' \
-                                           + df_bsdd_origine_poids['poids'].astype(str) + ' t)'
+    df_bsdd_origine_poids: pd.DataFrame = recus[['emitterCompanyAddress', 'poids']].copy()
+    df_bsdd_origine_poids['departement_origine'] = df_bsdd_origine_poids['emitterCompanyAddress'].str. \
+        extract(r"(\d{2})\d{3}")
+    df_bsdd_origine_poids = df_bsdd_origine_poids[['departement_origine', 'poids']]. \
+        groupby(by='departement_origine', as_index=False).sum()
+    df_bsdd_origine_poids['poids'] = df_bsdd_origine_poids['poids'].apply(round)
+    df_bsdd_origine_poids.sort_values(by='poids', ascending=True, inplace=True)
+    df_bsdd_origine_poids = df_bsdd_origine_poids.tail(10)
+    departements = pd.read_csv('app/assets/departement.csv', index_col='DEP')
+    df_bsdd_origine_poids = df_bsdd_origine_poids.merge(departements, left_on='departement_origine',
+                                                        right_index=True)
+    df_bsdd_origine_poids['LIBELLE'] = df_bsdd_origine_poids['LIBELLE'] + ' (' \
+                                       + df_bsdd_origine_poids['poids'].astype(str) + ' t)'
 
-        bsdd_emis_acceptation_mois = px.bar(
-            df_bsdd_acceptation_mois,
-            title="BSDD émis par mois et par acceptation",
-            y="id",
-            x="mois",
-            color="acceptation",
-            text="id",
-            labels={
-                "mois": "",
-                "id": "",
-            },
-        ).update_traces(textangle=0)
+    bsdd_emis_acceptation_mois = px.bar(
+        df_bsdd_acceptation_mois,
+        title="BSDD émis par mois et par acceptation",
+        y="id",
+        x="mois",
+        color="acceptation",
+        text="id",
+        labels={
+            "mois": "",
+            "id": "",
+        },
+    ).update_traces(textangle=0)
 
-        dechets_recus_emis_poids_mois = px.line(
-            bsdd_grouped_poids_mois,
-            y="poids",
-            x="mois",
-            color="origine",
-            symbol="origine",
-            title="Déchets entrant et sortant, en tonnes",
-            labels={"poids": "", "mois": "", "type": ""},
-            markers=True,
-            text="poids"
-        ).update_traces(textposition="top center")
+    dechets_recus_emis_poids_mois = px.line(
+        bsdd_grouped_poids_mois,
+        y="poids",
+        x="mois",
+        color="origine",
+        symbol="origine",
+        title="Déchets entrant et sortant, en tonnes",
+        labels={"poids": "", "mois": "", "type": ""},
+        markers=True,
+        text="poids"
+    ).update_traces(textposition="top center")
 
-        dechets_recus_poids_departement = px.bar(
-            df_bsdd_origine_poids,
-            title="Origine des déchets dangereux reçus (top 10)",
-            y="departement_origine",
-            x="poids",
-            text="LIBELLE",
-            labels={
-                "departement_origine": "Département d'origine",
-                "poids": "Poids (tonnes)",
-            },
-            # So that departements are not treated as numbers
-        ).update_yaxes(autotypenumbers='strict')
+    dechets_recus_poids_departement = px.bar(
+        df_bsdd_origine_poids,
+        title="Origine des déchets dangereux reçus (top 10)",
+        y="departement_origine",
+        x="poids",
+        text="LIBELLE",
+        labels={
+            "departement_origine": "Département d'origine",
+            "poids": "Poids (tonnes)",
+        },
+        # So that departements are not treated as numbers
+    ).update_yaxes(autotypenumbers='strict')
 
-        return {
-            "bsdd_graphiques_col": [
-                dbc.Col(
-                    [
-                        dcc.Graph(id='mois_quantités', config=extra_config, figure=dechets_recus_emis_poids_mois)
-                     ], width=6, lg=6),
-                dbc.Col(
-                    [
-                        dcc.Graph(id='mois_emis', config=extra_config, figure=bsdd_emis_acceptation_mois)
-                    ], width=6, lg=6
-                )
-            ],
-            "poids_departement_recus": dcc.Graph(id='poids_departement_recus', config=extra_config,
-                                                 figure=dechets_recus_poids_departement)
-        }
-    # No record in the BSDD dataframe
     return {
         "bsdd_graphiques_col": [
-            dbc.Col([
-                "Pas de bordereaux entrés dans Trackdéchets pour cet établissement sur la période, "
-                "ni comme émetteur, "
-                "ni comme destinataire."
-            ], width={'size': 4, 'offset': 4})
+            dbc.Col(
+                [
+                    dcc.Graph(id='mois_quantités', config=extra_config, figure=dechets_recus_emis_poids_mois)
+                ], width=6, lg=6),
+            dbc.Col(
+                [
+                    dcc.Graph(id='mois_emis', config=extra_config, figure=bsdd_emis_acceptation_mois)
+                ], width=6, lg=6
+            )
         ],
-        "poids_departement_recus": ""
+        "poids_departement_recus": dcc.Graph(id='poids_departement_recus', config=extra_config,
+                                             figure=dechets_recus_poids_departement)
     }
 
 
 @dash_app.callback(
     Output('bsdd_summary', 'children'),
-    Input('query-result', 'data')
-
+    Input('query-result', 'data'),
+    Input('siret', 'value')
 )
-def get_bsdd_summary(json_data: str) -> list:
+def get_bsdd_summary(json_data: str, siret: str) -> list:
+    if len(siret) != 14:
+        return []
+
     df: pd.DataFrame = pd.read_json(json_data, orient='split', dtype={'poids': float})
 
-    if df.index.size > 0:
-        emis = df.query('origine == "émis"')
-        recus = df.query('origine == "reçus"')
-        poids_emis_float = emis["poids"].sum()
-        poids_recu_float = recus["poids"].sum()
+    if df.index.size == 0:
+        return []
 
-        return [
-            html.H4('BSD dangereux sur la période'),
-            html.P([
-                'Poids émis : ', format_number_str(poids_emis_float), ' t',
-                html.Br(),
-                'Poids reçu : ', format_number_str(poids_recu_float), ' t',
-                html.Br(),
-                'Stock théorique sur la période : ', format_number_str(poids_recu_float - poids_emis_float), ' t']),
+    emis = df.query('origine == "émis"')
+    recus = df.query('origine == "reçus"')
+    poids_emis_float = emis["poids"].sum()
+    poids_recu_float = recus["poids"].sum()
 
-            html.P([
-                'BSDD émis : ', format_number_str(emis.index.size),
-                html.Br(),
-                'BSDD reçus : ', format_number_str(recus.index.size),
-            ])
-        ]
-    return []
+    return [
+        html.H4('BSD dangereux sur la période'),
+        html.P([
+            'Poids émis : ', format_number_str(poids_emis_float), ' t',
+            html.Br(),
+            'Poids reçu : ', format_number_str(poids_recu_float), ' t',
+            html.Br(),
+            'Stock théorique sur la période : ', format_number_str(poids_recu_float - poids_emis_float), ' t']),
+
+        html.P([
+            'BSDD émis : ', format_number_str(emis.index.size),
+            html.Br(),
+            'BSDD reçus : ', format_number_str(recus.index.size),
+        ])
+    ]
 
 
 # ******************************************************
@@ -234,6 +248,13 @@ def get_bsdd_summary(json_data: str) -> list:
         company_name=Output('company_name', 'children')),
 )
 def get_company_data(siret: str) -> dict:
+    # The entered SIRET is not 14 char long
+    if len(siret) != 14:
+        return {
+            'company_details': html.Div([html.Strong("Le numéro SIRET doit comporter 14 chiffres")]),
+            'company_name': ""
+        }
+
     df_company_query = pd.read_sql_query(
         'SELECT "Company"."siret", "Company"."name", "Company"."createdAt", "Company"."address",'
         '"Company"."contactPhone",'
@@ -247,20 +268,45 @@ def get_company_data(siret: str) -> dict:
         con=engine,
     )
 
+    # Dataframe has no record, établissement is not in Trackdéchets
+    if df_company_query.index.size == 0:
+        return {
+            'company_details': html.Div([]),
+            'company_name': "Établissement non inscrit dans Trackdéchets"
+        }
+
+    def make_agreement_list(df: pd.DataFrame) -> list[html.Li]:
+        agreement_list = []
+
+        for i, nom, number, department, validityLimit in df.itertuples():
+            if number is not None:
+                validity = ", valide jusqu'au " + dt.strftime(validityLimit, '%d %b %Y') \
+                    if validityLimit is not None else ''
+                agreement = html.Li(nom + number + f' (préf. {department})' + validity)
+                agreement_list.append(agreement)
+        if len(agreement_list) == 0:
+            agreement_list = [html.Li('néant')]
+        return agreement_list
+
+    etab = df_company_query.iloc[0]
+
     df_agreement_query = pd.read_sql_query(
-        'SELECT \'Agré. démolisseur VHU n°\' as "nom", agrement."agrementNumber" as "number", agrement."department",'
+        'SELECT \'Agré. démolisseur VHU n°\' as "nom", agrement."agrementNumber" as "number", '
+        'agrement."department",'
         'CAST(NULL AS timestamp) as "validityLimit" from "default$default"."VhuAgrement" agrement '
         'RIGHT JOIN "default$default"."Company" company '
         'ON agrement."id" = company."vhuAgrementDemolisseurId" '
         f'WHERE company."siret" = \'{siret}\' '
         'union '
-        'SELECT \'Agré. broyeur VHU n°\' as "nom", agrement."agrementNumber" as "number", agrement."department",'
+        'SELECT \'Agré. broyeur VHU n°\' as "nom", agrement."agrementNumber" as "number", '
+        'agrement."department",'
         ' CAST(NULL AS timestamp) as "validityLimit" from "default$default"."VhuAgrement" agrement '
         'RIGHT JOIN "default$default"."Company" company '
         'ON agrement."id" = company."vhuAgrementBroyeurId" '
         f'WHERE company."siret" = \'{siret}\' '
         'union '
-        'SELECT \'Récep. transporteur n°\' as "nom", agrement."receiptNumber" as "number", agrement."department",'
+        'SELECT \'Récep. transporteur n°\' as "nom", agrement."receiptNumber" as "number", '
+        'agrement."department",'
         ' agrement."validityLimit" as "validityLimit" from "default$default"."TransporterReceipt" agrement '
         'RIGHT JOIN "default$default"."Company" company '
         'ON agrement."id" = company."transporterReceiptId"'
@@ -278,23 +324,7 @@ def get_company_data(siret: str) -> dict:
         'ON agrement."id" = company."brokerReceiptId" '
         f'WHERE company."siret" = \'{siret}\'',
         con=engine)
-
-    def make_agreement_list() -> list[html.Li]:
-        df = df_agreement_query
-        agreement_list = []
-
-        for i, nom, number, department, validityLimit in df.itertuples():
-            if number is not None:
-                validity = ", valide jusqu'au " + dt.strftime(validityLimit, '%d %b %Y') \
-                    if validityLimit is not None else ''
-                agreement = html.Li(nom + number + f' (préf. {department})' + validity)
-                agreement_list.append(agreement)
-        if len(agreement_list) == 0:
-            agreement_list = [html.Li('néant')]
-        return agreement_list
-
-    etab = df_company_query.iloc[0]
-
+    # Dataframe has records, return information
     return {
         'company_details': html.Div([
             html.P(f'le {dt.strftime(today, "%d %b %Y à %H:%M")}'),
@@ -308,7 +338,7 @@ def get_company_data(siret: str) -> dict:
                 "L'entreprise a déclaré sur Trackdéchets disposer des "
                 "agréments/récepissés "
                 "suivants :"),
-            html.Ul(make_agreement_list()),
+            html.Ul(make_agreement_list(df_agreement_query)),
             html.P('Données pour la période du ' +
                    dt.strftime(date_n_days_ago, "%d %b %Y")
                    + ' à aujourd\'hui (' + getenv("TIME_PERIOD_M") + ' derniers mois).',
