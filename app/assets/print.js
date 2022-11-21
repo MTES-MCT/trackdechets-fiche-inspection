d4b9bdc65f6f2c7a4ead2bb2131a405ba7b33ed5
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2022-11-21 16:14:39 +0100

    Adds a script to prepare page for printing
    New logic to prepare page for printing including
    a JS script that apply CSS transformation before printing.
    Also changes the Dash HTML Layout and add DSFR elements.

--- app/assets/print.js
+++ app/assets/print.js
@@ -1,10 +1,35 @@
-function resizeForPrinting(e) {
+function relayoutPlotly() {
     document.querySelectorAll('.dash-graph').forEach(function (element) {
+        console.log(element)
 
         const box = element.getBoundingClientRect()
-        Plotly.relayout(element, { width: box.width, height: box.height, autosize: true })
+        Plotly.relayout(element.getElementsByClassName("js-plotly-plot")[0], { width: box.width, height: box.height, autosize: true })
 
     })
 }
 
-window.addEventListener('beforeprint', resizeForPrinting)
+function resizeForPrinting(e) {
+    console.log('Before print');
+    const layoutContainer = document.querySelector("#layout-container")
+    layoutContainer.className = "printing"
+
+    relayoutPlotly()
+    setTimeout(print, 500)
+
+}
+
+function resetLayout() {
+    const layoutContainer = document.querySelector("#layout-container")
+    layoutContainer.className = ""
+    setTimeout(relayoutPlotly, 900)
+}
+
+window.addEventListener('afterprint', resetLayout)
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        const printButton = document.querySelector("#print-button")
+        printButton.addEventListener("click", resizeForPrinting)
+        printButton.disabled = false
+    }, 1000)
+})
