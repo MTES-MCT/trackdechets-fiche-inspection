0beec118e49bb0a6f635f2066aec7e95fd1cd5de
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2022-11-22 10:34:06 +0100

    Adds long callback and loading spinner

--- app/app.py
+++ app/app.py
@@ -7,6 +7,8 @@ from os import getenv
 import dash
 import dash_auth
 import dash_bootstrap_components as dbc
+import diskcache
+
 
 from app.layout.layout_factory import get_layout
 
@@ -15,12 +17,16 @@ locale.setlocale(locale.LC_ALL, "fr_FR")
 auth_data = {"trackdechets": getenv("APP_PASSWORD")}
 external_scripts = ["https://cdn.plot.ly/plotly-locale-fr-latest.js"]
 
+cache = diskcache.Cache("./cache")
+background_callback_manager = dash.DiskcacheManager(cache)
+
 # Use [dbc.themes.BOOTSTRAP] to import the full Bootstrap CSS
 dash_app = dash.Dash(
     __name__,
     title="Fiche d'inspection",
     external_stylesheets=[dbc.themes.GRID],
     external_scripts=external_scripts,
+    long_callback_manager=background_callback_manager,
 )
 
 auth = dash_auth.BasicAuth(dash_app, auth_data)
